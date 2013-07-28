d9f4ceb3cc647191068c6df7c1eb684576d56046
Attila Györffy <attila@attilagyorffy.com>
2013-07-28 20:21:46 +0200

    Remove unnecessary sleep.
    See Capybara.default_wait_time https://github.com/jnicklas/capybara#asynchronous-javascript-ajax-and-friends

--- spec/features/search_feature_spec.rb
+++ spec/features/search_feature_spec.rb
@@ -14,7 +14,6 @@ feature "Search", %q{
   scenario "finding the answer to the question of life" do
     fill_in 'gbqfq', :with => 'the answer to the question of life'
     click_button 'gbqfb'
-    sleep 1
     page.should have_selector "ol#rso"
     page.should have_text '42'
   end
